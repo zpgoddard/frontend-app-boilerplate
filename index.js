ca4bdc8df0e3da8fe53af38aacb5c0cb0ba19a4f
Zac-Goddard-Codices <zpgoddard@gmail.com>
2026-02-13 23:44:15 +0000

    module manager nomanclature changes

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-// Load Tamagui config first so createTamagui/setConfig run before any Tamagui components.
-// Required for web to avoid "Can't find Tamagui configuration".
-import './tamagui.config';
+import './tamagui.config'; // Load Tamagui config first so createTamagui/setConfig run before any Tamagui components.
 
 import { registerRootComponent } from 'expo';
 import App from './src/App';
